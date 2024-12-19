c760020682521eedfe235868263d9fabb41aa14e
saikumartanakala <119582244+saikumartanakala@users.noreply.github.com>
2024-12-19 18:28:43 +0530

    Create userData.tf

---
+++ userData.tf
@@ -0,0 +1,38 @@
+resource "aws_security_group" "docker" {
+  name = "docker"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 800
+    to_port     = 800
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "task" {
+ami           = "ami-id"
+instance_type = "t2.micro"
+user_data     = "${file("linux.sh")}"
+tags = {
+Name = "docker_git_httpd"
+}
+}
